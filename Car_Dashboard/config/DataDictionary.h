8b6cbd43e178cc90cc7d8d27f06e84d70b73ee6b
unknown <tuananh1997@gmail.com>
2024-12-04 16:55:20 +0700

    reformat dd

--- Car_Dashboard/config/DataDictionary.h
+++ Car_Dashboard/config/DataDictionary.h
@@ -24,13 +24,13 @@ constexpr char GETRequest_Offline[] =               "/GetOfflineData.ashx?token=
 ///GetOnlineData.ashx?token=;gnss=gps,glo,qzss,gal;datatype=eph,alm,aux,pos;lat=10.815329;lon=106.657337;alt=10.000000;pacc=10000.000000
 #define USE_DEFAULT_LOCATION 1
 #ifdef USE_HOCHIMINH    //10.81532915851147, 106.6573371901137
-constexpr char cachePos[] = "lat=10.81532915851147;lon=106.6573371901137;alt=10.000000;pacc=50000.000000";
+constexpr char cachePos[] =                         "lat=10.81532915851147;lon=106.6573371901137;alt=10.000000;pacc=50000.000000";
 #endif
 #ifdef USE_VUNGTAU      //10.367506732991703, 107.08795322622392
-constexpr char cachePos[] = "lat=10.367506732991703;lon=107.08795322622392;alt=7.000000;pacc=50000.000000";
+constexpr char cachePos[] =                         "lat=10.367506732991703;lon=107.08795322622392;alt=7.000000;pacc=50000.000000";
 #endif
 #ifdef USE_DEFAULT_LOCATION //10.81532915851147, 106.6573371901137, 100km radius
-constexpr char cachePos[] = "lat=10.81532915851147;lon=106.6573371901137;alt=10.000000;pacc=100000.000000";
+constexpr char cachePos[] =                         "lat=10.81532915851147;lon=106.6573371901137;alt=10.000000;pacc=100000.000000";
 #endif
 
 constexpr int MAX_XAXIS_ANGLE =                     180;
